42e5db3d3d063884a0d76615d63a7f73ea866681
sidorsz <gamingsidorus@gmail.com>
2025-11-27 05:44:27 +0700

    Add files via upload

---
+++ Supabase.js
@@ -0,0 +1,7 @@
+// START: Inisialisasi klien Supabase
+      const supabase = window.supabase.createClient(
+        "https://mnobniomzmjgpyyqartz.supabase.co",
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ub2JuaW9tem1qZ3B5eXFhcnR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU3NDQ0NzYsImV4cCI6MjA2MTMyMDQ3Nn0.N1w5Ro7TGWu5zdTHhNCyop-iMTl7So7h6UwZfuybx1k"
+      );
+      
+      // END: Inisialisasi klien Supabases
